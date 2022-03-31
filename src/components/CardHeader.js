63884868d33db528dcf93d9e71e476fda42847ed
naglenda31 <77177688+naglenda31@users.noreply.github.com>
2022-03-31 19:12:14 -0400

    updated profile image border radius

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -6,7 +6,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const CardHeader = () => {
     return (
         <div className="card-header flex flex-col gap-2 text-center">
-            <img src={profilePicture} alt="profile" className="w-full h-64 rounded-t-md" />
+            <img src={profilePicture} alt="profile" className="w-full h-64 rounded-t-md md:rounded-tr-none" />
             <div className="flex flex-col px-6 xl:px-0">
                 <h1 className="text-2xl font-semibold">Glenda Negron</h1>
                 <h2 className="text-sm text-rose-400">Frontend Developer</h2>
